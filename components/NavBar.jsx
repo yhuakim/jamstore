f644acce2b127449deabedc420b1e71bad4d97b3
ARINZE JOACHIM OJILI <47617723+yhuakim@users.noreply.github.com>
2022-11-05 06:17:30 +0100

    Links Update

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -30,15 +30,15 @@ const NavBar = () => {
 
             <nav className="bg-slate-100 px-2 sm:px-4 py-2.5 sticky w-full z-20 top-0 left-0 border-b border-gray-200">
                 <div className="container flex flex-wrap justify-between items-center mx-auto">
-                    <a href="/" className="flex items-center text-black">
+                    <Link href="/" className="flex items-center text-black">
                         <ShoppingCartIcon className='w-9 h-9' />
                         <span class="self-center text-xl font-semibold whitespace-nowrap">JAMStore</span>
-                    </a>
+                    </Link>
                     <div class="flex md:order-2">
-                        <a href='/cart' className="text-black hover:text-white hover:bg-black focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium relative rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0">
+                        <Link href='/cart' className="text-black hover:text-white hover:bg-black focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium relative rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0">
                             <ShoppingBagIcon className='w-6 h-6' />
                             <small className='bg-black rounded-full text-white text-[8px] font-semibold absolute px-1 top-0' >{totalQuantity}</small>
-                        </a>
+                        </Link>
                         <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-sticky" aria-expanded="false">
                             <span className="sr-only">Open main menu</span>
                             <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
--- components/Products.jsx
+++ components/Products.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link"
 
 /* grid grid - cols - 3 gap - 4 w - 96 ml - 4 pt - 4
  */
@@ -11,18 +12,18 @@ const Products = ({ products }) => {
                             <b className={`${!prod.discount_id ? 'none' : 'absolute top-2 right-2 text-xs text-red-300 bg-red-100 p-1'}`}>{prod.discount_id && prod.discount_id.isActive ? `${prod.discount_id.discount_percent * 100}%` : ''}</b>
                         </small>
                         <div className="w-52">
-                            <a href={`/product/${prod.id}`} className="">
+                            <Link href={`/product/${prod.id}`} className="">
                                 <img className='object-cover w-52 h-48' src={prod.imageUrl} alt="" />
-                            </a>
+                            </Link>
                         </div>
                         <div className="card-info py-2 px-2">
-                            <a href={`/product/${prod.id}`}>
+                            <Link href={`/product/${prod.id}`}>
                                 <h3>{prod.name}</h3>
                                 <p>
                                     <b>{prod.discount_id && prod.discount_id.isActive ? (prod.price - (prod.price * prod.discount_id.discount_percent)) : prod.price}</b> <br />
                                     <small><s className={`${!prod.discount_id ? 'none' : 'text-gray-500 decoration-gray-500'}`}>{prod.discount_id && prod.discount_id.isActive ? prod.price : ''}</s></small>
                                 </p>
-                            </a>
+                            </Link>
                         </div>
                     </div>
                 ))
